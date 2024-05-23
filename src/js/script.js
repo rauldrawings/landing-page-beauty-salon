4433fab71dbfcb77b00dd1473257146602f116ef
rauldrawings <raul.cabral.goes@gmail.com>
2024-05-23 17:00:26 -0300

    5:00 pm

--- src/js/script.js
+++ src/js/script.js
@@ -5,3 +5,10 @@ for (const element of toggle){
         nav.classList.toggle('show')
     })
 }
+//Quando clicar em um item do menu-bar, esconder o menu//
+const links = document.querySelectorAll('nav ul li a')
+for (const link of links) {
+    link.addEventListener ('click', function (){
+        nav.classList.remove('show')
+    })
+}
