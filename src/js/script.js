d21efcbf97477dff888a3f2489e8e1be7192f953
rauldrawings <raul.cabral.goes@gmail.com>
2024-05-23 16:48:32 -0300

    4:48 pm

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+for (const element of toggle){
+    element.addEventListener('click', function(){
+        nav.classList.toggle('show')
+    })
+}
